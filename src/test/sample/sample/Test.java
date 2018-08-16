b87e3d0150024aff064af0e5b7e00e8bb891a7f7
Kiran Kumar <vkirankumar3090@gmail.com>
2018-08-16 13:23:09 +0530

    Update Test.java

--- src/test/sample/sample/Test.java
+++ src/test/sample/sample/Test.java
@@ -5,6 +5,8 @@ import com.Zapi.Utilities.ZephyrTestCaseManagementCloud;
 public class Test {
 
 	public static void main(String[] args) {
+		//Prerequisite - Update the application.properties file.
+		//Just call this method in your after suite and pass the cucumber json file path as parameter this will take care of updating the status in Jira.
 		ZephyrTestCaseManagementCloud tcm = new ZephyrTestCaseManagementCloud();
 		tcm.updateTestCaseAndStepStatus("report.json");
 	}
